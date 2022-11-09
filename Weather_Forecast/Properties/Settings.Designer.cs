2673d46a6532825f6eba105951fb509b1d692c7e
Kalichhe <jhondh2003@gmail.com>
2022-11-09 14:47:07 -0500

    Ni

--- Weather_Forecast/Properties/Settings.Designer.cs
+++ Weather_Forecast/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Weather_Forecast.Properties {
                 return ((string)(this["Weather_ForecastConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\WeatherForeca" +
+            "stDB.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string WeatherForecastDBConnectionString {
+            get {
+                return ((string)(this["WeatherForecastDBConnectionString"]));
+            }
+        }
     }
 }
